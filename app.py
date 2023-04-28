68f30de8e8c8ad6c363282b1c55e7cf68a569bae
Kamiar Radnosrati <kamiar.radnosrati@ericsson.com>
2023-04-28 14:34:29 +0200

    Add member name to each note created

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ class Member(db.Model):
     password = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    notes = db.relationship('Note', backref='member', lazy=True)
 
     def __repr__(self):
         return f"Member('{self.name}', '{self.email}')"
